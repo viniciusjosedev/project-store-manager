002c957fd7e632327e81abab9ce194bbf688ba91
Vinicius José <viniciusjosedev@gmail.com>
2023-04-27 09:09:55 -0300

    more tests

--- src/models/connection.js
+++ src/models/connection.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
-  port: 3306,
+  port: process.env.MYSQL_PORT || 3306,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: 'StoreManager',
+  database: process.env.MYSQL_DATABASE || 'StoreManager',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
--- tests/unit/controllers/productsController.test.js
+++ tests/unit/controllers/productsController.test.js
@@ -1,64 +1,64 @@
-// const { expect } = require('chai');
-// const sinon = require('sinon');
+const { expect } = require('chai');
+const sinon = require('sinon');
 
-// const { productsController } = require('../../../src/controllers/index')
-// const { productsService } = require('../../../src/services/index');
+const { productsController } = require('../../../src/controllers/index')
+const { productsService } = require('../../../src/services/index');
 
-// const { resultFindAllMock, resultFindByIdMock } = require('./mocks/productsControllerMocks');
+const { resultFindAllMock, resultFindByIdMock } = require('./mocks/productsControllerMocks');
 
-// describe('All tests from productsController', function () {
-//   afterEach(() => sinon.restore());
-//   describe('Tests from function findAll', function () {
-//     it('Test with sucess', async function () {
-//       sinon.stub(productsService, 'findAll').resolves(resultFindAllMock);
-//       const req = {};
-//       const res = {
-//         status: () => { },
-//         json: () => { },
-//       };
+describe('All tests from productsController', function () {
+  afterEach(() => sinon.restore());
+  describe('Tests from function findAll', function () {
+    it('Test with sucess', async function () {
+      sinon.stub(productsService, 'findAll').resolves(resultFindAllMock);
+      const req = {};
+      const res = {
+        status: () => { },
+        json: () => { },
+      };
 
-//       sinon.stub(res, 'status').returns(res);
-//       sinon.stub(res, 'json').returns(resultFindAllMock);
+      sinon.stub(res, 'status').returns(res);
+      sinon.stub(res, 'json').returns(resultFindAllMock);
 
-//       const result = await productsController.findAll(req, res);
+      const result = await productsController.findAll(req, res);
 
-//       expect(result).to.be.deep.equal(resultFindAllMock)
-//     })
-//   })
-//   describe('Tests from function findById', function () {
-//     it('Test with sucess', async function () {
-//       sinon.stub(productsService, 'findById').resolves(resultFindByIdMock);
-//       const req = { 
-//         params: { id: '1' }
-//        };
-//       const res = {
-//         status: () => { },
-//         json: () => { },
-//       };
+      expect(result).to.be.deep.equal(resultFindAllMock)
+    })
+  })
+  describe('Tests from function findById', function () {
+    it('Test with sucess', async function () {
+      sinon.stub(productsService, 'findById').resolves(resultFindByIdMock);
+      const req = { 
+        params: { id: '1' }
+       };
+      const res = {
+        status: () => { },
+        json: () => { },
+      };
 
-//       sinon.stub(res, 'status').returns(res);
-//       sinon.stub(res, 'json').returns(resultFindByIdMock);
+      sinon.stub(res, 'status').returns(res);
+      sinon.stub(res, 'json').returns(resultFindByIdMock);
 
-//       const result = await productsController.findById(req, res);
-//       expect(result).to.be.deep.equal(resultFindByIdMock)
-//     })
-//     it('Test without sucess', async function () {
-//       sinon.stub(productsService, 'findById').resolves(undefined);
-//       const req = {
-//         params: { id: '1' }
-//       };
-//       const res = {
-//         status: () => { },
-//         json: () => { },
-//       };
+      const result = await productsController.findById(req, res);
+      expect(result).to.be.deep.equal(resultFindByIdMock)
+    })
+    it('Test without sucess', async function () {
+      sinon.stub(productsService, 'findById').resolves(undefined);
+      const req = {
+        params: { id: '1' }
+      };
+      const res = {
+        status: () => { },
+        json: () => { },
+      };
 
-//       sinon.stub(res, 'status').returns(res);
-//       sinon.stub(res, 'json').returns({message: "Product not found"});
+      sinon.stub(res, 'status').returns(res);
+      sinon.stub(res, 'json').returns({message: "Product not found"});
 
-//       const result = await productsController.findById(req, res);
-//       expect(result).to.be.deep.equal({message: "Product not found"})
-//     })
-//   })
-// })
+      const result = await productsController.findById(req, res);
+      expect(result).to.be.deep.equal({message: "Product not found"})
+    })
+  })
+})
 
 
