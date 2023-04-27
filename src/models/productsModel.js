5f48c8899ff33d459d5e9916711b2dd24f658252
Vinicius José <viniciusjosedev@gmail.com>
2023-04-27 08:48:48 -0300

    more updates

--- src/models/productsModel.js
+++ src/models/productsModel.js
@@ -2,7 +2,7 @@ const connection = require('./connection');
 
 const findAll = async () => {
   const [result] = await connection.execute(
-    'SELECT * FROM products;',
+    'SELECT * FROM products',
   );
   return result;
 };
