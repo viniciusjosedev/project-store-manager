4b0aa77b3937711baf594160611d187f378c2b40
Vinicius José <viniciusjosedev@gmail.com>
2023-04-27 09:42:44 -0300

    create: tests createProduct from productsModel

--- src/models/productsModel.js
+++ src/models/productsModel.js
@@ -14,7 +14,15 @@ const findById = async (id) => {
   return result;
 };
 
+const createProduct = async ({ name }) => {
+  const [{ insertId }] = await connection.execute(
+    'INSERT INTO products(name) VALUES', [name],
+  );
+  return insertId;
+};
+
 module.exports = {
   findAll,
   findById,
+  createProduct,
 };
