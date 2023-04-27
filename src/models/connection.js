2982537f6313780abf162bc51167ff00beb24f72
Vinicius José <viniciusjosedev@gmail.com>
2023-04-26 23:59:19 -0300

    fix: error connection.js

--- src/models/connection.js
+++ src/models/connection.js
@@ -5,7 +5,6 @@ const connection = mysql.createPool({
   port: process.env.MYSQL_PORT || 3306,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: 'StoreManager',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
--- src/models/productsModel.js
+++ src/models/productsModel.js
@@ -2,14 +2,14 @@ const connection = require('./connection');
 
 const findAll = async () => {
   const [result] = await connection.execute(
-    'SELECT * FROM products;',
+    'SELECT * FROM StoreManager.products;',
   );
   return result;
 };
 
 const findById = async (id) => {
   const [[result]] = await connection.execute(
-    'SELECT * FROM products WHERE id = ?', [id],
+    'SELECT * FROM StoreManager.products WHERE id = ?', [id],
   );
   return result;
 };
