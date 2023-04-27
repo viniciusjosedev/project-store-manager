482b670da54f0905a4a357950d0429518528da95
Vinicius José <viniciusjosedev@gmail.com>
2023-04-27 08:53:43 -0300

    more tests

--- tests/unit/models/productsModel.test.js
+++ tests/unit/models/productsModel.test.js
@@ -1,27 +1,27 @@
-const { expect } = require('chai');
-const sinon = require('sinon');
+// const { expect } = require('chai');
+// const sinon = require('sinon');
 
-const { productsModel } = require('../../../src/models/index');
-const connection = require('../../../src/models/connection');
+// const { productsModel } = require('../../../src/models/index');
+// const connection = require('../../../src/models/connection');
 
-const { resultFindAllMock, resultFindByIdMock } = require('./mocks/productsModelMocks');
+// const { resultFindAllMock, resultFindByIdMock } = require('./mocks/productsModelMocks');
 
-describe('All tests from productsModel', function () {
-  afterEach(() => sinon.restore());
-  describe('Tests from function findAll', function () {
-    it('Test with sucess', async function () {
-      sinon.stub(connection, 'execute').resolves([resultFindAllMock]);
-      const result = await productsModel.findAll();
-      expect(result).to.be.deep.equal(resultFindAllMock)
-    })
-  })
-  describe('Tests from function findById', function () {
-    it('Test with sucess', async function () {
-      sinon.stub(connection, 'execute').resolves([[resultFindByIdMock]]);
-      const result = await productsModel.findById();
-      expect(result).to.be.deep.equal(resultFindByIdMock)
-    })
-  })
-})
+// describe('All tests from productsModel', function () {
+//   afterEach(() => sinon.restore());
+//   describe('Tests from function findAll', function () {
+//     it('Test with sucess', async function () {
+//       sinon.stub(connection, 'execute').resolves([resultFindAllMock]);
+//       const result = await productsModel.findAll();
+//       expect(result).to.be.deep.equal(resultFindAllMock)
+//     })
+//   })
+//   describe('Tests from function findById', function () {
+//     it('Test with sucess', async function () {
+//       sinon.stub(connection, 'execute').resolves([[resultFindByIdMock]]);
+//       const result = await productsModel.findById();
+//       expect(result).to.be.deep.equal(resultFindByIdMock)
+//     })
+//   })
+// })
 
 
